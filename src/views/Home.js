827ab560253e6bc12cb3ddfa4e92b4fe5f4dcd3d
KAYITARE-C <ani7kayitare@gmail.com>
2022-01-26 20:14:05 +0200

    adding signup link

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,6 +6,17 @@ const Home = () => {
   return (
     
   <HomeLayout>
+    <div className="text">
+    <h1>
+    Lost  &  Found  Document.
+    </h1>
+    <p>Welcome!!!!  Now you can be able to easily found your lost document 
+by click on sign-up Button of this website</p>
+<button>Sign-up</button>
+
+    </div>
+
+    
 
   </HomeLayout>
   )
