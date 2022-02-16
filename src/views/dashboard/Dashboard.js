fc85b2a57f1717042313b0b83b0438eb7a3d5ee7
KAYITARES <cyntkayitare@example.com>
2022-02-16 16:11:19 +0200

    adding style

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,49 +1,52 @@
 import React from "react";
-import DashboardLayout from "../../components/dashboardlayout.js"
-import { BarChart } from 'reaviz';
-import { MenuOutlined } from '@ant-design/icons'
-import logo from '../../assets/img/logo.png'
-import {Card } from 'antd';
+import DashboardLayout from "../../components/dashboardlayout.js";
+// import { BarChart } from "reaviz";
+import { MenuOutlined } from "@ant-design/icons";
+import logo from "../../assets/img/logo.png";
+import { Card } from "antd";
 // import "./dashboardLayout.css";
 
 const statistics = [
-    {
-      key: 'Found',
-      data: 5
-    },
-    {
-      key: 'Lost',
-      data: 14
-    },
-    {
-      key: 'pending',
-      data: 15
-    },
-    {
-      key: 'total',
-      data: 30
-    },
-  ];
-  
-  const DashboardInfos=() => {
-      return(
-        <Card>
-        <div className="DashInfos">
-           <div className="header">
-                  {/* <div className="menu"> <MenuOutlined /> </div> */}
-                  <div className="logo-division"> <img src={logo}></img></div>
-                  {/* <div className="logout">log out</div> */}
-              </div>
-          <DashboardLayout>
-              <div className="chart-container">
-    <div style={{ margin: '55px', textAlign: 'center' }}>
-      <BarChart width={350} height={250} top={100} data={statistics}  />
+  {
+    key: "Found",
+    data: 5,
+  },
+  {
+    key: "Lost",
+    data: 14,
+  },
+  {
+    key: "pending",
+    data: 15,
+  },
+  {
+    key: "total",
+    data: 30,
+  },
+];
+
+const DashboardInfos = () => {
+  return (
+    <Card>
+      <div className="DashInfos">
+        <div className="header">
+          {/* <div className="menu"> <MenuOutlined /> </div> */}
+          <div className="logo-division">
+            {" "}
+            <img src={logo}></img>
+          </div>
+          {/* <div className="logout">log out</div> */}
+        </div>
+        <DashboardLayout>
+          <div className="chart-container">
+            <div style={{ margin: "55px", textAlign: "center" }}>
+              {/* <BarChart width={350} height={250} top={100} data={statistics} /> */}
+            </div>
+          </div>
+        </DashboardLayout>
       </div>
-    </div>
-    </DashboardLayout>
-    </div>
     </Card>
-  )};
+  );
+};
 
-  export default DashboardInfos;
-
+export default DashboardInfos;
